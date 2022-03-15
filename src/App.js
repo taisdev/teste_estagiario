35aec6cd540b3b8fad4de8abd7390e85c6a92ef6
Taís Oliveira <taisdossantos_16@hotmail.com>
2022-03-15 13:02:15 -0300

    Atualização do arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import { firebase, auth } from './firebase/db'
 function App() {
-  const handleClickButtonLogin = async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-  const result = await auth.signInWithPopup(provider);
-  };
   
   return (
     <div className="App">
-      <main>
-        <button onClick={handleClickButtonLogin}>Login</button>
-      </main>
+     
     </div>
   );
 }
